35d7dbbe2a5a017f3ca0aefab1c88b8f921153a4
Ngota <ngotacoding@gmail.com>
2023-08-08 09:54:45 +0300

    Initial commit

---
+++ src/model.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super().__init__()
+
+        # YOUR CODE HERE
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=256, out_channels=1000, kernel_size=3, stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten()
+        )
+
+        # Define the classifier layers
+        self.classifier = nn.Sequential(
+            nn.Linear(1000, 7000),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(7000, num_classes),
+            nn.Softmax(dim=1)
+        )
+        # Define a CNN architecture. Remember to use the variable num_classes
+        # to size appropriately the output of your classifier, and if you use
+        # the Dropout layer, use the variable "dropout" to indicate how much
+        # to use (like nn.Dropout(p=dropout))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # YOUR CODE HERE: process the input tensor through the
+        # feature extractor, the pooling and the final linear
+        # layers (if appropriate for the architecture chosen)
+        features = self.feature_extractor(x)
+        output = self.classifier(features)
+        return x
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = dataiter.next()
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
